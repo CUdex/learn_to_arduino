eb9ae4780c6717588ec661613dfc2832a05f3358
류찬울 <ryuchan-ul@lyuchan-ul-ui-MacBookPro.local>
2021-03-02 15:16:20 +0900

    led test

--- led_control/led_control.ino
+++ led_control/led_control.ino
@@ -1,9 +1,63 @@
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(2,HIGH);
+  delay(1000);
+  digitalWrite(2,LOW);
+  digitalWrite(3,HIGH);
+  delay(1000);
+  digitalWrite(3,LOW);
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(4,LOW);
+  digitalWrite(5,HIGH);
+  delay(1000);
+  digitalWrite(5,LOW);
+  digitalWrite(6,HIGH);
+  delay(1000);
+  digitalWrite(6,LOW);
+  delay(3000);
 
+  digitalWrite(2,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,HIGH);
+  delay(1000);
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  delay(2000);
+
+  for(int i = 0; i < 3; i++)
+  {
+    if (i % 2 == 0)
+    {
+      digitalWrite(2,HIGH);
+      digitalWrite(4,HIGH);
+      digitalWrite(6,HIGH);
+      delay(1000);
+      digitalWrite(2,LOW);
+      digitalWrite(4,LOW);
+      digitalWrite(6,LOW);
+    }
+    else
+    {
+      digitalWrite(3,HIGH);
+      digitalWrite(5,HIGH);
+      delay(1000);
+      digitalWrite(3,LOW);
+      digitalWrite(5,LOW);
+    }
+    delay(1000);
+  }
 }
